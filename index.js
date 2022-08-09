1d8537e66a13b6027f39e2a2ad09b6e847c7da3f
Steven <dombrowskis153@yahoo.com>
2022-08-09 00:45:17 -0400

    Addition of Tabs

--- index.js
+++ index.js
@@ -86,10 +86,27 @@ http.onload = function(){
                </div> 
             </div>`;
         }
-        document.querySelector(".dogsData").innerHTML = output;
+        document.querySelector(".active").innerHTML = output;
     }
 };
 
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
 
 
 
