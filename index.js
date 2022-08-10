c0e99bf2b0760540699bbfbcdaa6b20b34b454e1
Steven <dombrowskis153@yahoo.com>
2022-08-09 23:08:36 -0400

    Dog API addition

--- index.js
+++ index.js
@@ -125,3 +125,44 @@ function dark(){
   };
 
 
+  const fetchDogBreeds = async () => {
+
+    const response = await fetch('https://api.thedogapi.com/v1/breeds');
+    const dogBreeds = await response.json();
+    populateDogSelect(dogBreeds);
+  
+  }
+  
+  const populateDogSelect = (breeds) => {
+    const select = document.querySelector('.breed-select');
+    const breedOptions = breeds.map(breed => {
+      const option = document.createElement('option');
+      option.text = breed.name;
+      option.value = breed.id;
+      return option;
+  
+    })
+    breedOptions.forEach(breedOption => {
+      select.appendChild(breedOption);
+    })
+  }
+  
+  const fillImage = (imageUrl) => {
+    document.querySelector('#dogimage').setAttribute('src', imageUrl);
+  }
+  
+  const getDogByBreed = async (breedId) => {
+    const [data] = await fetch('https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=' + breedId).then((data) => data.json())
+    const {url: imageUrl, breeds} = data;
+    fillImage(imageUrl);
+    
+  
+  }
+  
+  const changeDog = () => {
+  
+    console.log(event.target.value)
+    getDogByBreed(event.target.value);
+    
+  }
+  fetchDogBreeds()
